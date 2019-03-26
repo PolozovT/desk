03d81e4c09e0c9ff32f815cdc29b72ab525868e3
PolozovT <polozovtima@rambler.ru>
2019-03-26 15:38:47 +0300

    added ToDo component

---
+++ src/components/ToDo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../ToDo.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="ToDo">
+                <header className="ToDo-header">
+                    <h1>
+                        ToDo
+                    </h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import ToDo from './components/ToDo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToDo />, document.getElementById('ToDo'));
+
 
 serviceWorker.unregister();
