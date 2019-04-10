fe63ecf83a3b3d5e5480dbb0adf0d372c7093889
PolozovT <polozovtima@rambler.ru>
2019-04-10 11:12:12 +0300

    start adding state

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import '../styles/ToDo.css';
 
 class ToDo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: props.defaultOpen
+        }
+    }
+
     render() {
         return (
             <div className="ToDo">
                 <input type="text" />
                 <input type="button" value="add"/>
+                <button onClick={this.handleClick}>close</button>
             </div>
         )
+    }
+
+    handleClick = () => {
+        console.log('---','clicked');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 }
 
